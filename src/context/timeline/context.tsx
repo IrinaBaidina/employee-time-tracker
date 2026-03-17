4c657136bd5c56c175d4da310a976c0944f6dbca
baidinairina2304 <baidinairina2304@gmail.com>
2026-03-17 15:22:35 +0300

    1

--- src/context/timeline/context.tsx
+++
@@ -1,5 +0,0 @@
-import { createSafeContext } from '@/utils/contexts/createSafeContext';
-import { TimelineContext } from './interfaces';
-
-export const [TimelineContextProviderBase, useTimeline] =
-  createSafeContext<TimelineContext>('TimelineContext');
